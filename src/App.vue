32f42f46e914f36c4a8f332d015eda47df60ee83
Vladislav Kozlenko <vladislavko@softwareplanet.uk.com>
2022-10-14 16:30:05 +0300

    Fix

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/ten-cards-test">Test(10)</router-link> |
-    <router-link to="/one-hundred-cards-test">Test(100)</router-link> |
-    <router-link to="/one-thousand-cards-test">Test(1000)</router-link> |
-    <router-link to="/five-thousand-cards-test">Test(5000)</router-link> |
-    <router-link to="/ten-thousand-cards-test">Test(10000)</router-link> |
-    <router-link to="/twenty-thousand-cards-test">Test(20000)</router-link> |
-    <router-link to="/thirty-thousand-cards-test">Test(30000)</router-link> |
-    <router-link to="/fifty-thousand-cards-test">Test(50000)</router-link> |||
-    <router-link to="/ten-thousand-cards-conditional-test">Test-conditional(10000)</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/ten-cards-test">Test(10)</router-link> |
+      <router-link to="/one-hundred-cards-test">Test(100)</router-link> |
+      <router-link to="/one-thousand-cards-test">Test(1000)</router-link> |
+      <router-link to="/five-thousand-cards-test">Test(5000)</router-link> |
+      <router-link to="/ten-thousand-cards-test">Test(10000)</router-link> |
+      <router-link to="/twenty-thousand-cards-test">Test(20000)</router-link> |
+      <router-link to="/thirty-thousand-cards-test">Test(30000)</router-link> |
+      <router-link to="/fifty-thousand-cards-test">Test(50000)</router-link> |||
+      <router-link to="/ten-thousand-cards-conditional-test">Test-conditional(10000)</router-link>
+    </nav>
+    <router-view/>
+  </div>
 </template>
 
 <style lang="scss">
